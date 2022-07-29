0c8aaa067b59715b2231aa36b319f40d9d365fa6
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-07-29 01:01:42 -0300

    feat: add server

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { userRouter, depositRouter, withdrawRouter } from './routes/index.js'
+import authorizationMiddleware from './middlewares/authorization.js'
+import errorHandler from './middlewares/error-handler.js'
+
+
+const server = express()
+server.use(express.json())
+const PORT = 3333
+
+server.use('/', userRouter)
+
+// server.use(authorizationMiddleware)
+
+server.use('/entradas', depositRouter)
+server.use('/saidas', withdrawRouter)
+
+server.use(errorHandler)
+
+server.listen(PORT, () => {
+  console.log(`\nServer started on port ${PORT}!`)
+})
